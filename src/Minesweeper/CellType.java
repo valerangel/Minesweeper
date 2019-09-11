6c7ee612ca8b2213450f610affacc8b507b637bf
valerangel <angelvarrera@hotmail.com>
2019-09-12 01:02:53 +0200

    Created

--- src/Minesweeper/CellType.java
+++ src/Minesweeper/CellType.java
@@ -4,7 +4,7 @@ public enum CellType {
 
     ONE(), TWO(), THREE(), FOUR(),
     FIVE(), SIX(), SEVEN(), EIGHT(),
-    UNTAPPED(), BOMB(), FLAG_BOMB(), FLAG_NO_BOMB,
+    UNTAPPED(), BOMB(), FLAG_BOMB(), FLAG_NO_BOMB(),
     EMPTY(), NULL(), SUN();
 
 
